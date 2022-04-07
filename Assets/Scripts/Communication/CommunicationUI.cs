9a9b7f5ea170dfbec618427c54b4a8d4d9b32b1d
Ryan <ryanjvigus@gmail.com>
2022-04-07 20:46:38 +0800

    shit

--- Assets/Scripts/Communication/CommunicationUI.cs
+++ Assets/Scripts/Communication/CommunicationUI.cs
@@ -12,13 +12,14 @@ public class CommunicationUI : MonoBehaviour
     public LexiconEntry[] categories;
 
     public GameObject button, phraseText, blankSpace, wordSelector;
-    public Transform UIContainer, phraseSelectPanel, phraseContainer, phrasePanel, categorySelectPanel, wordSelectPanel;
+    public Transform UIContainer, oldOrNewPanel, historyPanel, phraseSelectPanel, phraseContainer, phrasePanel, categorySelectPanel, wordSelectPanel;
     //public UnityEngine.EventSystems.EventSystem eventSystem;
 
     private int activeWordIndex;
     private int activePanel = -1;
     //private string[] history = new string[3];
     private Queue<string> history = new Queue<string> ();
+    private Button[] historyButtons;
     private PhotonView photonView;
     private BubbleManager dogBubbles, humanBubbles;
 
@@ -26,6 +27,8 @@ public class CommunicationUI : MonoBehaviour
     {
         photonView = GetComponent<PhotonView> ();
         photonView.ViewID = 69;
+
+        //historyButtons = historyPanel.GetComponentsInChildren<Button> ();
         
         foreach (string phrase in phrases)
             PlaceButton (phrase, phraseSelectPanel, () => OnPhraseSelected (phrase));
@@ -40,7 +43,7 @@ public class CommunicationUI : MonoBehaviour
 
         SelectFirstButton (phraseSelectPanel);
 
-        EventManager.I.OnPlayerSpawned += OnPlayersSpawned;
+        //EventManager.I.OnPlayerSpawned += OnPlayersSpawned;
     }
 
     private void OnPlayersSpawned (PlayerSpecies player, GameObject gameObject)
@@ -86,6 +89,16 @@ public class CommunicationUI : MonoBehaviour
             case PlayerSpecies.Human: humanBubbles.SpawnBubble (message);
                 break;
         }
+    }
+
+    public void OnOldNewSelected (bool newPhrase)
+    {
+        SetPanel (newPhrase ? 1 : 2);
+    }
+
+    public void OnHistorySelected (int button)
+    {
+
     }
 
     private void OnPhraseSelected (string phrase)
@@ -139,26 +152,32 @@ public class CommunicationUI : MonoBehaviour
         print (activePanel);
         if (activePanel < 0)
             activePanel = -1;
-        else if (activePanel > 3)
-            activePanel = 1;
+        else if (activePanel > 5)
+            activePanel = 3;
 
         switch (activePanel)
         {
-            case 0: SelectFirstButton (phraseSelectPanel);
+            case 0: SelectFirstButton (oldOrNewPanel);
                 break;
-            case 1: phraseContainer.GetChild (activeWordIndex).GetComponent<Button> ().Select ();
+            case 1: SelectFirstButton (historyPanel);
                 break;
-            case 2: SelectFirstButton (categorySelectPanel);
+            case 2: SelectFirstButton (phraseSelectPanel);
                 break;
-            case 3: SelectFirstButton (wordSelectPanel);
+            case 3: phraseContainer.GetChild (activeWordIndex).GetComponent<Button> ().Select ();
+                break;
+            case 4: SelectFirstButton (categorySelectPanel);
+                break;
+            case 5: SelectFirstButton (wordSelectPanel);
                 break;
         }
 
         UIContainer.gameObject.SetActive (activePanel >= 0);
-        phraseSelectPanel.gameObject.SetActive (activePanel == 0);
-        phrasePanel.gameObject.SetActive (activePanel == 1);
-        categorySelectPanel.gameObject.SetActive (activePanel == 2);
-        wordSelectPanel.gameObject.SetActive (activePanel == 3);
+        oldOrNewPanel.gameObject.SetActive (activePanel >= 0);
+        historyPanel.gameObject.SetActive (activePanel >= 1);
+        phraseSelectPanel.gameObject.SetActive (activePanel == 2);
+        phrasePanel.gameObject.SetActive (activePanel == 3);
+        categorySelectPanel.gameObject.SetActive (activePanel == 4);
+        wordSelectPanel.gameObject.SetActive (activePanel == 5);
     }
 
     private void PlacePhrase (string phrase)
